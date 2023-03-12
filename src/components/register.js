5a58ffa651debd196dad5015e80ae57c0ecfeef0
Daniel Holguin <daniel.holguin@correounivalle.edu.co>
2023-03-12 12:02:39 -0500

    bug fix 2.0 regexPassword

--- src/components/register.js
+++ src/components/register.js
@@ -43,6 +43,7 @@ export default function Register(){
         let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
         let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
         //let regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; //La contraseña debe tener mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.
+       // eslint-disable-next-line react-hooks/exhaustive-deps
         let regexPassword = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" //Minimum eight characters, at least one letter and one number:
         if (!nombre.trim()) 
         {
